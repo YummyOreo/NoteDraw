55abad9a5d0c44d8a40b74b0b553c5b498add33e
YummyOreo <bobgim20@gmail.com>
2021-11-28 19:51:57 -0600

    adds buttons

--- src/main.go
+++ src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
@@ -10,13 +13,20 @@ func main() {
 
 	w := a.NewWindow("My Window")
 
-	with := 200
-	height := 400
+	with := 300
+	height := 300
 
 	multiH := 2
-	multiW := 3
+	multiW := 2
 
 	w.Resize(fyne.NewSize(float32(with*multiW), float32(height*multiH)))
+
+	buttonOne := widget.NewButton("name", func() {
+		fmt.Println("works")
+	})
+
+	w.SetContent(buttonOne)
+
 	w.ShowAndRun()
 
 }
